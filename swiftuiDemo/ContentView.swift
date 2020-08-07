cdd7f8ca86725ff1af4837d1e71fecfc8b69709e
akwarrio2 <akwarrio87@users.noreply.github.com>
2020-08-07 12:50:02 +0530

    Initial Commit

---
+++ swiftuiDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuiDemo
+//
+//  Created by daffolapmac-131 on 07/08/20.
+//  Copyright © 2020 daffolapmac-131. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
